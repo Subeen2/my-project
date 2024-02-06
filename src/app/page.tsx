df97aaa87a472a316a8492b818b3c3cba827a932
Subeen2 <snb0768@naver.com>
2024-02-06 21:49:34 +0900

    feat: initial setting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,16 @@
 import styles from "./page.module.css";
 
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="container mx-auto">
+      <main>
+        <div className="space-y-4">
+          <h1 className="text-center text-5xl">NextJS 13 Blog</h1>
+          <p className="text-center text-xl">
+            Welcome to a dynamic markdown blog using NextJS 13.
+          </p>
+        </div>
+      </main>
+    </div>
+  );
 }
