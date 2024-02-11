797879e06eb1e6214a8eabeff592050228ef316a
Subeen2 <snb0768@naver.com>
2024-02-11 14:28:54 +0900

    feat: create skeleton page

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,10 @@
+import { NextPage } from "next";
+import React from "react";
+
+interface IPageProps {}
+
+const Page: NextPage = (props: IPageProps) => {
+  return <div>!?!??</div>;
+};
+
+export default Page;
--- src/skeleton/index.tsx
+++ src/skeleton/index.tsx
@@ -4,7 +4,7 @@ import React from "react";
 interface IPageProps {}
 
 const Page: NextPage = (props: IPageProps) => {
-  return <div>!?!??</div>;
+  return <div></div>;
 };
 
 export default Page;
